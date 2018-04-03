be0d5e587beb623382fa339781a045ebf0d4e5aa
Roman S <romasar86@gmail.com>
2018-04-03 10:42:30 +0300

    add docker file

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ module.exports = {
     name:"books-ms",
     port: 3001,
     mongodb: {
-        host: "127.0.0.1",
+        host: "mongodb",
         port: 27017,
         dbName: "experimental"
     }
